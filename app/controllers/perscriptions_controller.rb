fbada4327b57ec2fb684f1ec720dc682f273dbbc
nickwells <wellsnick11@yahoo.com>
2022-12-31 13:59:14 -0800

    Sessions Controller Done

--- app/controllers/perscriptions_controller.rb
+++ app/controllers/perscriptions_controller.rb
@@ -1,2 +1,9 @@
 class PerscriptionsController < ApplicationController
+    skip_before_action :authorize
+    skip_before_action :is_doc
+
+    def index 
+        render json: Perscription.all, status: :ok
+    end
+    
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+    skip_before_action
+    skip_before_action
+
+    def doclogin
+        user = Doctor.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[:current_user] = user.id
+            render json: user, status: :ok
+        else
+            render json: { error: ["Invalid email or password"] }, status: :unauthorized
+        end
+    end
+
+    def patientlogin
+        user = Patient.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[:current_user] = user.id
+            render json: user, status: :ok
+        else
+            render json: { error: ["Invalid email or password"] }, status: :unauthorized
+        end
+    end
+
+    def logout
+        session.delete :current_user
+    end
+
 end
