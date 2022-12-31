f9b8e7f81baf2522acb3988d3ef2d451ec81e885
nickwells <wellsnick11@yahoo.com>
2022-12-31 14:05:26 -0800

    Added the attributes for the serializers

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,3 +1,9 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :startDate, :title, :location, :notes, :doctor_id, :patient_id, :patient
+
+  def patient
+    object.patient.name
+  end
+  
+
 end
--- app/serializers/department_serializer.rb
+++ app/serializers/department_serializer.rb
@@ -1,3 +1,5 @@
 class DepartmentSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :image
+
+  has_many :doctors
 end
