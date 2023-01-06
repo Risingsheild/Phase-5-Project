34fa10d738bcbce33aa0462974e1902d656f45d8
nickwells <wellsnick11@yahoo.com>
2023-01-05 19:58:45 -0800

    Prettier save/Doctors Slice done

--- client/src/TableSlices/appointmentsSlice.js
+++ client/src/TableSlices/appointmentsSlice.js
@@ -1,98 +1,103 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 //getting all appointments
-export const fetchAppointments = createAsyncThunk("appointments/fetchAppointments", () => {
+export const fetchAppointments = createAsyncThunk(
+  "appointments/fetchAppointments", () => {
     return fetch("/appointments")
-        .then((r) => r.json())
-        .then((data) => data)
-})
+      .then((r) => r.json())
+      .then((data) => data);
+  }
+);
 
 //creating a new appointment
-  export const createAppointment = createAsyncThunk(
-    "appointments/createAppointment",
-    async (newAppt) => {
-      return fetch(`/appointments`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newAppt),
-      }).then((res) => res.json());
-    }
-  );
-
+export const createAppointment = createAsyncThunk(
+  "appointments/createAppointment",
+  async (newAppt) => {
+    return fetch(`/appointments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newAppt),
+    }).then((res) => res.json());
+  }
+);
 
 // updating appointments
 export const updateAppointment = createAsyncThunk(
-    "newAppointments/updateAppointments",
-    async (appt) => {
-      return fetch(`/appointments/${appt.id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": 'application/json'
-        },
-        body: JSON.stringify(appt),
-      })
-      .then((res) => res.json())
-    }
-  );
+  "newAppointments/updateAppointments",
+  async (appt) => {
+    return fetch(`/appointments/${appt.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(appt),
+    }).then((res) => res.json());
+  }
+);
 
-  //delete appointments
-  export const deleteAppointment = createAsyncThunk(
-    "appointment/deleteAppointment",
-    async (apptId) => {
-      fetch(`/appointments/${apptId}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    }
-  );
+//delete appointments
+export const deleteAppointment = createAsyncThunk(
+  "appointment/deleteAppointment",
+  async (apptId) => {
+    fetch(`/appointments/${apptId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+);
 
-  const appointmentsSlice = createSlice({
-    name: 'appointments',
-    initialState: {
-        entities: [],
-        status: 'idle'
+const appointmentsSlice = createSlice({
+  name: "appointments",
+  initialState: {
+    entities: [],
+    status: "idle",
+  },
+  reducers: {
+    onDeleteAppointment(state, action) {
+      const index = state.entities.findIndex((a) => a.id === action.payload);
+      state.entities.splice(index, 1);
     },
-    reducers: {
-        onDeleteAppointment(state, action) {
-            const index = state.entities.findIndex(a => a.id === action.payload)
-            state.entities.splice(index, 1)
-        },
-        onUpdateAppointment(state, action) {
-            state.entities = state.entities.map(appt => appt.id === action.payload.id ? {...appt, ...action.payload} : appt )
-        }
+    onUpdateAppointment(state, action) {
+      state.entities = state.entities.map((appt) =>
+        appt.id === action.payload.id ? { ...appt, ...action.payload } : appt
+      );
+    },
+  },
+
+  extraReducers: {
+    [fetchAppointments.pending](state) {
+      state.status = "loading Appointments";
     },
 
-    extraReducers: {
-        [fetchAppointments.pending](state){
-            state.status = 'loading Appointments';
-        },
+    [fetchAppointments.fulfilled](state, action) {
+      state.entities = action.payload;
+      state.status = "idle";
+    },
 
-        [fetchAppointments.fulfilled](state, action) {
-            state.entities = action.payload;
-            state.status = 'idle';
-          },
+    [createAppointment.fulfilled](state, action) {
+      state.entities = [...state.entities, action.payload];
+    },
 
-        [createAppointment.fulfilled](state, action){
-            state.entities = [...state.entities, action.payload]
-        },
+    [updateAppointment.fulfilled](state, action) {
+      state.entities.map((appt) =>
+        appt.id === action.payload.id ? { ...appt, ...action.payload } : appt
+      );
+    },
 
-        [updateAppointment.fulfilled](state, action) {
-            state.entities.map(appt => appt.id === action.payload.id ? {...appt, ...action.payload} : appt)
-        },
+    [deleteAppointment.fulfilled](state, action) {
+      state.entities = state.entities.filter(
+        (appt) => appt.id !== action.payload
+      );
+    },
+  },
+});
 
-        [deleteAppointment.fulfilled](state, action){
-            state.entities = state.entities.filter(
-                (appt) => appt.id !== action.payload
-            );
-        },
-    } 
-  })
+export const { onDeleteAppointment, onUpdateAppointment } =
+  appointmentsSlice.actions;
 
-  export const {onDeleteAppointment, onUpdateAppointment} = appointmentsSlice.actions
-
-  export default appointmentsSlice.reducer
+export default appointmentsSlice.reducer;
--- client/src/TableSlices/departmentsSlice.js
+++ client/src/TableSlices/departmentsSlice.js
@@ -1,46 +1,48 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchDepartments = createAsyncThunk("departments/fetchDepartments", () => {
+export const fetchDepartments = createAsyncThunk(
+  "departments/fetchDepartments", () => {
     return fetch("/departments")
       .then((response) => response.json())
       .then((data) => data);
-  });
+  }
+);
 
-  export const fetchDepartment = createAsyncThunk("departments/fetchDepartment", (id) => {
+export const fetchDepartment = createAsyncThunk(
+  "departments/fetchDepartment", (id) => {
     return fetch(`/departments/${id}`)
       .then((response) => response.json())
       .then((data) => data);
-  });
-  
-  const departmentsSlice = createSlice({
-    name: 'departments',
-    initialState: {
-      entities: [],
-      status: 'idle'
+  }
+);
+
+const departmentsSlice = createSlice({
+  name: "departments",
+  initialState: {
+    entities: [],
+    status: "idle",
+  },
+
+  reducers: {},
+  extraReducers: {
+    [fetchDepartments.pending](state) {
+      state.status = "loading";
     },
-    
-    reducers: {
-  
+
+    [fetchDepartments.fulfilled](state, action) {
+      state.entities = action.payload;
+      state.status = "idle";
     },
-    extraReducers: {
-      [fetchDepartments.pending](state) {
-        state.status = 'loading';
-      },
 
-      [fetchDepartments.fulfilled](state, action) {
-        state.entities = action.payload;
-        state.status = 'idle';
-      },
+    [fetchDepartment.pending](state) {
+      state.status = "loading";
+    },
 
-      [fetchDepartment.pending](state) {
-        state.status = 'loading';
-      },
+    [fetchDepartment.fulfilled](state, action) {
+      state.entities = action.payload;
+      state.status = "idle";
+    },
+  },
+});
 
-      [fetchDepartment.fulfilled](state, action) {
-        state.entities = action.payload;
-        state.status = 'idle';
-      }
-    }
-  })
-  
-  export default departmentsSlice.reducer
+export default departmentsSlice.reducer;
