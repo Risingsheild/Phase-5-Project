8539a636e86986122202490399ce17d8abb47f28
nickwells <wellsnick11@yahoo.com>
2023-03-04 18:03:59 -0800

    departments sections is completed

--- client/src/Components/DepartmentDoctor/DepartmentDoctor.js
+++ client/src/Components/DepartmentDoctor/DepartmentDoctor.js
@@ -1,45 +1,41 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+
 import { useParams } from "react-router-dom";
 
+import "./DepartmentDoctor.css";
 
 function DepartmentDoctor() {
+  const [dept, setDept] = useState([]);
 
-  const [dept, setDept] = useState([])
-  
-  const navigate = useNavigate;
-  const {id} = useParams()
+  const { id } = useParams();
 
+  useEffect(() => {
+    fetch(`/departments/${id}`)
+      .then((r) => r.json())
+      .then((data) => setDept(data));
+  }, []);
+  console.log(id);
 
-useEffect(() => { 
-  fetch(`/departments/${id}`)
-  .then((r) => r.json())
-  .then((data) => setDept(data))
- 
-
-},[])
-   console.log(id);
-
-
-return (
+  return (
     <div className="doctor-profile">
       <h1 className="dept-title"> {dept.name}</h1>
       <img src={dept.image} alt="img" className="image" />
-      <div className="doc-list">
-        <h1> Department Doctors: </h1>
-  <h3>
-        {dept.doctors
-          ? dept.doctors.map((doc) => (
-              <li
-                key={doc.id}
-                onClick={() => navigate(`/doctors/${doc.id}`)}
-              >{`Dr. ${doc.name}, PharmD`}</li>
-            ))
-          : null}
+      <div>
+        <h1 style={{color: 'rgb(242, 194, 158)'}}> Department Doctors: </h1>
+        <h3 className="doc-container">
+          {dept.doctors
+            ? dept.doctors.map((doc) => (
+                <div className="doclist">
+                  <li key={doc.id} className='docinfo'>
+                    {`Dr. ${doc.name}, PharmD`} <br></br> {doc.bio} <p style={{fontStyle: 'italic', fontSize: '15px' }}>{doc.email}</p>
+                  </li>
+                </div>
+              ))
+            : null}
         </h3>
       </div>
     </div>
   );
 }
 
-export default DepartmentDoctor
+export default DepartmentDoctor;
--- client/src/Components/Doctors/Doctor.js
+++ client/src/Components/Doctors/Doctor.js
@@ -1,12 +1,55 @@
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CardContent from "@mui/material/CardContent";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import CardActions from "@mui/material/CardActions";
+import { styled } from "@mui/material/styles";
 
-function Doctor({doc}) {
-    return(
-        <div>
-            <h1>{`Dr. ${doc[0].name}, PharmD`}</h1>
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+function Doctor({ doc }) {
+  const [expanded, setExpanded] = useState(false);
+
+  function handleExpandedClick(e) {
+    e.preventDefault();
+    setExpanded(!expanded);
+  }
+
+  console.log('At the Doctors', doc);
+  return (
+    <div className="doc-container">
+      <Card className="doc-card">
+        <CardHeader title={`Dr. ${doc.name}`} subheader="PharmD" />
+        <CardActions disableSpacing>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandedClick}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
             <h3>{doc[0].bio}</h3>
             <h5> Contact me at: {doc[0].email}</h5>
-        </div> 
-    )
+          </CardContent>
+        </Collapse>
+      </Card>
+    </div>
+  );
 }
 
-export default Doctor
+export default Doctor;
